bedab03d397ea96a7aff0e9d03de5619c83e1b3a
xuac <qb2eweoe8@relay.firefox.com>
2022-02-19 11:14:53 +0000

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -22,6 +22,7 @@ const config = {
       algolia: {
         appId: "WYRH3S2A0X",
         apiKey: "7b1d879342446005dbe681114a1815e5",
+        indexName: "awesome_piracy",
       },
       autoCollapseSidebarCategories: true,
       navbar: {
